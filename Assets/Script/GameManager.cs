764c19ea7cfb0c7746a09eef3f82d07c85ab28a1
c-c-c-c <4c.mura@gmail.com>
2018-12-08 11:59:00 +0900

    no message

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -4,13 +4,43 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    static float restTime = 60;
+    static int score;
+    
+    
+
+    // Use this for initialization
+    void Start () {
+
+        Debug.Log("start");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        // 時間を引いていく
+
+        restTime -= Time.deltaTime;
+
+        if (restTime < 0)
+        {
+            restTime = 0;
+        }
+
+        
+
+        print( restTime ); 
+
+
+        // 
+
+    }
+
+    public void AddScore ( int addScore )
+    {
+        score += addScore; //ポイントを追加する
+        print(score);
+
+    }
+
 }
